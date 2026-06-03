e6692da9d378b62ca80f0a295a3a7a23b65737b9
SERGIO ANDRES SEPULVEDA VELIZ <88344205+sasepulveda2@users.noreply.github.com>
2026-06-03 01:22:19 -0400

    import de funciones

---
+++ platform/mixins/__init__.py
@@ -0,0 +1,25 @@
+from mixins.calibration_ui_mixin import CalibrationUIMixin
+from mixins.camera_mixin import CameraMixin
+from mixins.intensity_analysis_mixin import IntensityAnalysisMixin
+from mixins.attenuation_mixin import AttenuationMixin
+from mixins.grid_mixin import GridMixin
+from mixins.projection_mixin import ProjectionMixin
+from mixins.image_processing_mixin import ImageProcessingMixin
+from mixins.file_management_mixin import FileManagementMixin
+from mixins.preferences_mixin import PreferencesMixin
+from mixins.theme_mixin import ThemeMixin
+from mixins.ui_setup_mixin import UISetupMixin
+
+__all__ = [
+    "CalibrationUIMixin",
+    "CameraMixin",
+    "IntensityAnalysisMixin",
+    "AttenuationMixin",
+    "GridMixin",
+    "ProjectionMixin",
+    "ImageProcessingMixin",
+    "FileManagementMixin",
+    "PreferencesMixin",
+    "ThemeMixin",
+    "UISetupMixin",
+]
